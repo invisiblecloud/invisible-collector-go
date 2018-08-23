38a439c351555c155c6eab7700df901be8154601
ros <ros65536@gmail.com>
2018-08-23 17:18:45 +0100

    add IC test

--- src/invisibleCollector_test.go
+++ src/invisibleCollector_test.go
@@ -1,1 +1,14 @@
 package ic
+
+import "testing"
+
+const (
+	testApiKey = "aded"
+)
+
+func TestInvalidUri(t *testing.T) {
+	if _, err := NewInvisibleCollector(testApiKey, "ftp://123.23.23.23"); err == nil {
+		t.Fail()
+	}
+
+}
