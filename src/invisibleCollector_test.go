908d8527531cb35f25b463fd89e42f4383028f79
ros <ros65536@gmail.com>
2018-08-23 17:21:07 +0100

    add IC tests

--- src/invisibleCollector_test.go
+++ src/invisibleCollector_test.go
@@ -10,5 +10,16 @@ func TestInvalidUri(t *testing.T) {
 	if _, err := NewInvisibleCollector(testApiKey, "ftp://123.23.23.23"); err == nil {
 		t.Fail()
 	}
+}
 
+func TestInvalidApiKey(t *testing.T) {
+	if _, err := NewInvisibleCollector("  \t\n", IcAddress); err == nil {
+		t.Fail()
+	}
+}
+
+func TestNew(t *testing.T) {
+	if _, err := NewInvisibleCollector(testApiKey, IcAddress); err != nil {
+		t.Fail()
+	}
 }
